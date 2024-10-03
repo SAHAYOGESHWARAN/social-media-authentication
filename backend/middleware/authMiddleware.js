9856cfb18b07b905be00dfed3b5e1da70d35afad
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-03 16:26:59 +0530

    social

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -16,6 +16,7 @@ const protect = async (req, res, next) => {
       res.status(401).json({ message: 'Not authorized, token failed' });
     }
   }
+  
 
   if (!token) {
     res.status(401).json({ message: 'Not authorized, no token' });
