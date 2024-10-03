f522d41ac362969efb823d77c7c63fd25e3274f2
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-03 16:43:43 +0530

    social

--- backend/routes/twilioRoutes.js
+++ backend/routes/twilioRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { requestPasswordReset, resetPassword } = require('../controllers/twilioController');
+const { requestPasswordReset, resetPassword ,sendVerificationOTP, verifyPhoneNumber } = require('../controllers/twilioController');
 
 const router = express.Router();
 
@@ -8,5 +8,11 @@ router.post('/reset-password/request', requestPasswordReset);
 
 // Route to reset the password using OTP
 router.post('/reset-password', resetPassword);
+
+// Route to send OTP for phone verification
+router.post('/verify-phone/send', sendVerificationOTP);
+
+// Route to verify phone number using OTP
+router.post('/verify-phone', verifyPhoneNumber);
 
 module.exports = router;
