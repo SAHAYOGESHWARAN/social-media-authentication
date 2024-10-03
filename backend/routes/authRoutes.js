fbbb502db22b2e5b94bc84f180f9288750a4eb38
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-03 16:23:10 +0530

    social

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
+const protect = require('../middleware/authMiddleware');
+
+
 
 const router = express.Router();
 
@@ -9,4 +12,11 @@ router.post('/register', registerUser);
 // Login route
 router.post('/login', loginUser);
 
+
+// Example protected route
+router.get('/profile', protect, (req, res) => {
+    res.json({ message: 'This is a protected route' });
+  });
+
+  
 module.exports = router;
